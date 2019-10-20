29c13f72cef197cff56c8e1a8934958256c74d82
PS-Ddevil <sinhapurushottam911@gmail.com>
2019-10-20 13:43:55 +0530

    removed deployment error basename

---
+++ precache-manifest.d4a6027947ef1029ede4d9943c655727.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51c10f465eb92dd06dca86caed92c97b",
+    "url": "/test-series/index.html"
+  },
+  {
+    "revision": "064ce140e2caadba8010",
+    "url": "/test-series/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "52322f4d7f5d83cad4d3",
+    "url": "/test-series/static/css/main.e980b8c3.chunk.css"
+  },
+  {
+    "revision": "064ce140e2caadba8010",
+    "url": "/test-series/static/js/2.b501feb8.chunk.js"
+  },
+  {
+    "revision": "52322f4d7f5d83cad4d3",
+    "url": "/test-series/static/js/main.fc533fca.chunk.js"
+  },
+  {
+    "revision": "81ea9d02d0634f6dfe28",
+    "url": "/test-series/static/js/runtime-main.27ce2cd2.js"
+  },
+  {
+    "revision": "e9294754c69be2f48df687b5b3383322",
+    "url": "/test-series/static/media/nirmaan.e9294754.jpg"
+  }
+]);
